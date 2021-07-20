0fc2b032b6f10873aaf5e8cf63f9a446c87767f3
Dennis Concepción Martín <dennisconcepcionmartin@gmail.com>
2021-07-20 12:23:37 +0100

    Add haptics

---
+++ Simoleon/Functions/SimpleSuccess.swift
@@ -0,0 +1,13 @@
+//
+//  SimpleSuccess.swift
+//  Simoleon
+//
+//  Created by Dennis Concepción Martín on 20/07/2021.
+//
+
+import SwiftUI
+
+func simpleSuccess() {
+    let generator = UINotificationFeedbackGenerator()
+    generator.notificationOccurred(.success)
+}
