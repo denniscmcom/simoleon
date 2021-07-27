d22bc4c2f9d6d5e95d6b825fdc2248c9d2f264f2
Dennis Concepción Martín <dennisconcepcionmartin@gmail.com>
2021-07-27 18:05:28 +0100

    Added WatchOS target

--- Simoleon/Persistence.swift
+++ Simoleon/Persistence.swift
@@ -13,10 +13,6 @@ struct PersistenceController {
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
         
         for _ in 0..<10 {
             let favorite = Favorite(context: viewContext)
@@ -25,8 +21,11 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            /*
+             Replace this implementation with code to handle the error appropriately.
+             fatalError() causes the application to generate a crash log and terminate.
+             You should not use this function in a shipping application, although it may be useful during development.
+             */
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
@@ -44,8 +43,11 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                /*
+                 Replace this implementation with code to handle the error appropriately.
+                 fatalError() causes the application to generate a crash log and terminate.
+                 You should not use this function in a shipping application, although it may be useful during development.
+                 */
                 
                 /*
                  Typical reasons for an error here include:
